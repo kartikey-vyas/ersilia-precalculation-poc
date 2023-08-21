821ac90dae99226011dc5c2130f53c62bc205723
Kartikey Vyas <kartikeyvyas23@gmail.com>
2023-08-21 22:38:26 +1000

    add another test to test pre-push commit

---
+++ tests/test_models.py
@@ -0,0 +1,14 @@
+from ersilia_precalc_poc.models import Prediction
+
+
+def test_prediction():
+    fixture = {
+        "model_id": "a",
+        "input_key": "b",
+        "smiles": "C",
+        "output": 1.0
+    }
+
+    pred = Prediction(**fixture)
+
+    assert isinstance(pred, Prediction)
