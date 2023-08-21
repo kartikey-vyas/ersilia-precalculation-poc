731bc9be6c35d25b0fefc5e101ab00748e5c1c7a
Kartikey Vyas <kartikeyvyas23@gmail.com>
2023-08-21 22:39:19 +1000

    formatted via hooks :sunglasses:

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,12 +2,7 @@ from ersilia_precalc_poc.models import Prediction
 
 
 def test_prediction():
-    fixture = {
-        "model_id": "a",
-        "input_key": "b",
-        "smiles": "C",
-        "output": 1.0
-    }
+    fixture = {"model_id": "a", "input_key": "b", "smiles": "C", "output": 1.0}
 
     pred = Prediction(**fixture)
 
